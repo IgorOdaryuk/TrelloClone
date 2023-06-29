454c458858d266cd2a798fe96c1ad405001ea383
IgorOdaryuk <odaryukigor@gmail.com>
2023-06-29 13:41:22 +0300

    creating a project template

---
+++ TrelloClone/TrelloCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrelloCloneApp.swift
+//  TrelloClone
+//
+//  Created by Igor Odaryuk on 29.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct TrelloCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
