37646e9385821de2953da8a8c418b263e5796c9f
IgorOdaryuk <odaryukigor@gmail.com>
2023-06-30 11:24:47 +0300

    Some changes to the cardviews

--- TrelloClone/Views/BoardListView.swift
+++ TrelloClone/Views/BoardListView.swift
@@ -6,17 +6,21 @@
 //
 
 import SwiftUI
+import Introspect
 
 struct BoardListView: View {
     
     @ObservedObject var board: Board
     @StateObject var boardList: BoardList
     
+    @State var listHeight: CGFloat = 0
+    
     var body: some View {
         VStack(alignment: .leading, spacing: 16) {
             headerView
             listView
                 .listStyle(.plain)
+                .frame(maxHeight: listHeight)
             
             Button("+ Add card") {
                 
@@ -65,7 +69,8 @@ struct BoardListView: View {
             .listRowInsets(.init(top: 4, leading: 8,
                                 bottom: 4, trailing: 8))
             .listRowBackground(Color.clear)
-            
+            .introspectTableView { listHeight = $0.contentSize.height
+            }
         }
     }
 }
@@ -75,7 +80,7 @@ struct BoardListView_Previews: PreviewProvider {
     @StateObject static var board = Board.stub
     
     static var previews: some View {
-        BoardListView(board: board, boardList: board.lists[0])
+        BoardListView(board: board, boardList: board.lists[0], listHeight: 512)
             .previewLayout(.sizeThatFits)
             .frame(width: 300, height: 512)
     }
